6ee5400132ebdd68c7cb886cd4e83d24d809274c
Arthur Ferreira <arthur_sf@icloud.com>
2022-11-10 11:30:53 -0300

    Commit.

---
+++ Scripts/table-with-buttons.R
@@ -0,0 +1,23 @@
+create_dt <- function(x, title) {
+  DT::datatable(
+    x,
+    rownames = FALSE,
+    extensions = 'Buttons',
+    options = list(
+      pageLength = 4,
+      scrolX = F,
+      dom = 'Bftip',
+      searchHighlight = TRUE,
+      buttons = list(
+        list(extend = "copy"),
+        list(extend = "csv"),
+        list(
+          extend = 'pdf',
+          title = title,
+          filename = title
+        )
+      )
+    ),
+    escape = FALSE
+  )
+}
