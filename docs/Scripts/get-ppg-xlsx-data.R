a4154a5b3019f3eb1b08fca936693b67a358bcee
Arthur Ferreira <arthur_sf@icloud.com>
2022-11-10 11:34:16 -0300

    Commit.

---
+++ docs/Scripts/get-ppg-xlsx-data.R
@@ -0,0 +1,6 @@
+# list all directories to extract PPG data
+files.to.read <-
+  list.files(file.path(getwd(), "PPG"),
+            full.names = TRUE,
+            pattern = "xlsx",
+            recursive = FALSE)
---
+++ docs/Scripts/webqualis.R
@@ -0,0 +1,11 @@
+# read QUALIS xlsx file (download from https://sucupira.capes.gov.br/sucupira/public/consultas/coleta/veiculoPublicacaoQualis/listaConsultaGeralPeriodicos.jsf)
+qualis <-
+  data.frame(read_excel(
+    file.path(
+      "Metrics",
+      "Qualis",
+      "relatorio_producao_intelectual_Qualis_Periódicos.xlsx"
+    ),
+    sheet = 1,
+    col_types = c("text")
+  ))
